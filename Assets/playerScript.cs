1a2c1f0f3b0f4022f8f55be313861c2ef0fa5b85
theTinHatter <56143899+theTinHatter@users.noreply.github.com>
2021-02-17 22:17:22 -0500

    Rock Project

---
+++ Assets/playerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+
+    public GameObject rock;
+    private Vector3 rockPos;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rock.GetComponent<transform.position>();
+        rockPos = new Vector3(rock.transform.position.x, rock.transform.position.y, -2);
+        this.transform.position = rockPos;
+    }
+}
---
+++ Assets/rockScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rockScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public Vector3 pushForce; 
+    public Vector3 resetPos;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown){
+            rb.AddForce(pushForce);
+        }
+    }
+    
+    void OnTriggerEnter(Collider collider){
+        if(collider.gameObject.tag == "Reset"){
+            // this.transform.position = resetPos;
+            // pushForce -= pushForce;
+            // Debug.Log("Hit");
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
